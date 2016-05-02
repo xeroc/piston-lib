7397a88901265e35cdee025b517f4f6f69f4db6a
Fabian Schuh <mail@xeroc.org>
2016-05-02 14:47:52 +0200

    [Async] don't autoload new classes

--- steemapi/__init__.py
+++ steemapi/__init__.py
@@ -1,7 +1,6 @@
 from steemapi.steemwalletrpc import SteemWalletRPC
 from steemapi.steemnoderpc import SteemNodeRPC
 from steemapi.steemclient import SteemClient
-from steemapi.steemasyncclient import SteemAsyncClient
 
 __all__ = ['steemwalletrpc',
            'steemnoderpc'
