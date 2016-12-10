87a32a1b304a257d79b1122a46cc456dd12cefe0
furion <_@furion.me>
2016-12-10 11:12:49 +0100

    [amount,converter] removed redundant Converter class from amount.py, re-added caches

--- steem/converter.py
+++ steem/converter.py
@@ -1,7 +1,12 @@
 import math
 
+from werkzeug.contrib.cache import SimpleCache
+
 import steem as stm
+from steem.utils import simple_cache
 from .amount import Amount
+
+converter_cache = SimpleCache()
 
 
 class Converter(object):
@@ -15,6 +20,7 @@ class Converter(object):
 
         self.CONTENT_CONSTANT = 2000000000000
 
+    @simple_cache(converter_cache, timeout=5 * 60)
     def sbd_median_price(self):
         """ Obtain the sbd price as derived from the median over all
             witness feeds. Return value will be SBD
@@ -24,6 +30,7 @@ class Converter(object):
             Amount(self.steem.rpc.get_feed_history()['current_median_history']['quote']).amount
         )
 
+    @simple_cache(converter_cache, timeout=5 * 60)
     def steem_per_mvests(self):
         """ Obtain STEEM/MVESTS ratio
         """
@@ -79,7 +86,7 @@ class Converter(object):
         """
         return amount_sbd / self.sbd_median_price()
 
-    def sbd_to_shares(self, sbd_payout):
+    def sbd_to_rshares(self, sbd_payout):
         """ Obtain r-shares from SBD
 
             :param number sbd_payout: Amount of SBD
