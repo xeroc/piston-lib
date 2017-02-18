7daf126ecfcec243a0d81567d07a370284dc3468
furion <_@furion.me>
2017-02-18 13:37:45 +0100

    [steemdata] add base SteemData implementation

--- steem/__init__.py
+++ steem/__init__.py
@@ -12,4 +12,5 @@ __all__ = [
     "transactions",
     "witness",
     "instance",
+    "data",
 ]
---
+++ steem/data.py
@@ -0,0 +1,44 @@
+import logging
+
+log = logging.getLogger(__name__)
+try:
+    import pymongo
+    from pymongo.errors import ConnectionFailure
+except ModuleNotFoundError:
+    mongo_error = """pysteem does not ship with MongoDB driver by default. Please install it by running:\n
+    pip install pymongo
+    """
+    log.error(mongo_error)
+
+
+class SteemData(object):
+    def __init__(self,
+                 db_name='SteemData',
+                 host='steemit:steemit@mongo1.steemdata.com',
+                 port=27017):
+        try:
+            self.mongo_url = 'mongodb://%s:%s/%s' % (host, port, db_name)
+            client = pymongo.MongoClient(self.mongo_url)
+            self.db = client[db_name]
+
+        except ConnectionFailure as e:
+            log.error('Can not connect to MongoDB server: %s' % e)
+            raise
+        else:
+            self.load_collections()
+
+    def list_collections(self):
+        return self.db.collection_names()
+
+    def load_collections(self):
+        for coll in self.list_collections():
+            setattr(self, coll, self.db[coll])
+
+    def info(self):
+        # todo print detailed info and some stats
+        print(self.mongo_url)
+
+
+if __name__ == '__main__':
+    s = SteemData()
+    s.info()
