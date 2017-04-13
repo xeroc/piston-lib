9ab834f2bd4a27be2ce7b9163b0a012f68e1e7aa
Fabian Schuh <Fabian@chainsquad.com>
2017-04-13 11:18:10 +0200

    version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except LookupError:
     ascii = codecs.lookup('ascii')
     codecs.register(lambda name, enc=ascii: {True: enc}.get(name == 'mbcs'))
 
-VERSION = '0.5.2'
+VERSION = '0.5.3'
 
 setup(
     name='piston-lib',
